313c02bfb57a0bf117f016bf8baa6e7e453b9aa8
Anton Shchukin <a.a.shchukin@gmail.com>
2023-07-09 21:55:35 +0700

    page player wrapper

--- markup/scripts/_page.js
+++ markup/scripts/_page.js
@@ -1,14 +1,14 @@
 (function($) {
 
     $('.show-player').on('click', function () {
-        $('.page__player:nth-child(2)').show();
-        $('.page__handler-for-player-two').hide();
+        $('.player--two').show();
+        $('.show-player').hide();
         $('.page').addClass('page--player-2-active');
     });
 
     $('.hide-player').on('click', function () {
-        $('.page__player:nth-child(2)').hide();
-        $('.page__handler-for-player-two').show();
+        $('.player--two').hide();
+        $('.show-player').show();
         $('.page').removeClass('page--player-2-active');
     });
 
