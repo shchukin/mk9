dca884ec451dde4866b58f10c8d0c28ec668d20c
Anton Shchukin <a.a.shchukin@gmail.com>
2023-07-09 21:36:00 +0700

    draft player separator

--- markup/scripts/_page.js
+++ markup/scripts/_page.js
@@ -3,11 +3,13 @@
     $('.show-player').on('click', function () {
         $('.page__player:nth-child(2)').show();
         $('.page__handler-for-player-two').hide();
+        $('.page').addClass('page--player-2-active');
     });
 
     $('.hide-player').on('click', function () {
         $('.page__player:nth-child(2)').hide();
         $('.page__handler-for-player-two').show();
+        $('.page').removeClass('page--player-2-active');
     });
 
 })(jQuery);
