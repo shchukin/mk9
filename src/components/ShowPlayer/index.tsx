a47349f48260f69accd195e0865d736d9bec0653
Anton Shchukin <a.a.shchukin@gmail.com>
2023-07-14 13:21:56 +0700

    show player

---
+++ src/components/ShowPlayer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'src/components/ShowPlayer/style.css';
+
+const ShowPlayer: React.FC = () => {
+  return (
+    <button className="show-player" type="button">
+      Show player 2
+    </button>
+  )
+}
+
+export default ShowPlayer;
