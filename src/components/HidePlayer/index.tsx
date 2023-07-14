2b054d244c16a2c53a3fa84ce1beedd76abc1e27
Anton Shchukin <a.a.shchukin@gmail.com>
2023-07-14 13:22:54 +0700

    hide player

---
+++ src/components/HidePlayer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'src/components/HidePlayer/style.css';
+
+const HidePlayer: React.FC = () => {
+  return (
+    <button className="hide-player" type="button">
+      Hide player 2
+    </button>
+  )
+}
+
+export default HidePlayer;
