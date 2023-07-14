bc9417f2cf7a7ed1d7e05ef5db7feaf9514cca95
SEVERSTAL\sv.khrulev <sv.khrulev@severstal.com>
2023-07-14 16:06:50 +0300

    Complete 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { playerContext, playerOneContextValue, playerTwoContextValue } from 'src/context';
 import Player from 'src/components/Player';
-import { getDataAsync, selectWarriorsData, useAppSelector, useAppDispatch } from 'src/store';
+import { getDataAsync, selectWarriorsData, selectShowSecondPlayer, useAppSelector, useAppDispatch } from 'src/store';
 
 const App: React.FC = () => {
   const dispatch = useAppDispatch();
   const warriorsData = useAppSelector(selectWarriorsData);
+  const showSecondPlayer = useAppSelector(selectShowSecondPlayer);
 
   React.useEffect(() => {
     dispatch(getDataAsync())
@@ -24,14 +25,14 @@ const App: React.FC = () => {
   }
 
   return (
-    <React.Fragment>
+    <div className = { `page ${showSecondPlayer ? 'page--player-2-active' : ''}` }>
       <playerContext.Provider value = { playerOneContextValue }>
         <Player playerIndex='one' />
       </playerContext.Provider>
       <playerContext.Provider value = { playerTwoContextValue }>
         <Player playerIndex='two' />
       </playerContext.Provider>
-    </React.Fragment>
+    </div>
   );
 }
 
