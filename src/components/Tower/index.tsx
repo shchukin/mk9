18a25949fc25c189b94b2201d3c2c39f591faaed
Anton Shchukin <a.a.shchukin@gmail.com>
2023-07-14 13:12:00 +0700

    tower

---
+++ src/components/Tower/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import 'src/components/Tower/style.css';
+
+const Tower: React.FC = () => {
+  return (
+    <div className="tower">
+      <a className="tower__link" href="#">Scorpion</a>
+      <a className="tower__link" href="#">Liu Kang</a>
+      <a className="tower__link" href="#">Kung Lao</a>
+      <a className="tower__link" href="#">Sub-Zero</a>
+      <a className="tower__link" href="#">Sindel</a>
+      <a className="tower__link" href="#">Ermac</a>
+      <a className="tower__link" href="#">Reptile</a>
+      <a className="tower__link" href="#">Kitana</a>
+      <a className="tower__link" href="#">Johnny Cage</a>
+      <a className="tower__link" href="#">Jade</a>
+      <a className="tower__link" href="#">Mileena</a>
+      <a className="tower__link" href="#">Nightwolf</a>
+      <a className="tower__link tower__link--current" href="#">Cyrax</a>
+      <a className="tower__link" href="#">Noob</a>
+      <a className="tower__link" href="#">Smoke</a>
+      <a className="tower__link" href="#">Sektor</a>
+      <a className="tower__link" href="#">Sonya Blade</a>
+      <a className="tower__link" href="#">Jax</a>
+      <a className="tower__link" href="#">Kano</a>
+      <a className="tower__link" href="#">Stryker</a>
+      <a className="tower__link" href="#">Shang Tsung</a>
+      <a className="tower__link" href="#">Baraka</a>
+      <a className="tower__link" href="#">Kabal</a>
+      <a className="tower__link" href="#">Raiden</a>
+      <a className="tower__link" href="#">Kratos</a>
+      <a className="tower__link" href="#">Sheeva</a>
+      <a className="tower__link" href="#">Quan Chi</a>
+      <a className="tower__link" href="#">Cyber Sub-Zero</a>
+      <a className="tower__link" href="#">Freddy Krueger</a>
+      <a className="tower__link" href="#">Rain</a>
+      <a className="tower__link" href="#">Kenshi</a>
+      <a className="tower__link" href="#">Skarlet</a>
+    </div>
+  )
+}
+
+export default Tower;
